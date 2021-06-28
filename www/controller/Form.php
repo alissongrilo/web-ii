4476641ac78e761f94897433057f9cc4fa7963fd
alissongrilo <alissonmiguel197@gmail.com>
2021-06-28 15:04:01 -0300

    CRUD V2 Editar On  28/06

--- www/controller/Form.php
+++ www/controller/Form.php
@@ -8,6 +8,10 @@ class Form
   public function controller()
   {
     $form = new Template("view/form.html");
+    $form->set("id", "");
+    $form->set("titulo", "");
+    $form->set("autores", "");
+    $form->set("descricao", "");
     $retorno["msg"] = $form->saida();
     return $retorno;
   }
@@ -21,11 +25,20 @@ class Form
         $autores = $conexao->quote($_POST["autores"]);
         $descricao = $conexao->quote($_POST["descricao"]);
         $crud = new Crud();
-        $retorno = $crud->insert(
-          "projetos",
-          "titulo,autores,descricao",
-          "{$titulo},{$autores},{$descricao}"
-        );
+        if (empty($_POST["id"])) {
+          $retorno = $crud->insert(
+            "projetos",
+            "titulo,autores,descricao",
+            "{$titulo},{$autores},{$descricao}"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $retorno = $crud->update(
+            "projetos",
+            "titulo={$titulo}, autores={$autores}, descricao={$descricao}",
+            "id={$id}"
+          );
+        }
       } catch (Exception $e) {
         $retorno["msg"] = "Ocorreu um erro! " . $e->getMessage();
         $retorno["erro"] = TRUE;
